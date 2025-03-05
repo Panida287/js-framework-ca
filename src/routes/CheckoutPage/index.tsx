62131e7cbf560dea3efabf24867879eab005ff03
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-03-05 20:41:05 +0100

    feat: added checkoutComplete flag
    added checkoutComplete flag in local storage so checkout/success can only be accessible by checkout button

--- src/routes/CheckoutPage/index.tsx
+++ src/routes/CheckoutPage/index.tsx
@@ -2,12 +2,24 @@ import {CartItems} from "../../components/Cart";
 import {useCartStore} from "../../store/cartStore";
 import {Button, BackToShopBtn} from "../../components/Button";
 import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export default function CartPage() {
     const {cart, clearCart} = useCartStore();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const checkoutComplete = localStorage.getItem("checkoutComplete");
+
+        if (!checkoutComplete) {
+            navigate("/");
+        } else {
+            localStorage.removeItem("checkoutComplete");
+        }
+    }, [navigate]);
+
     const handleCheckout = () => {
+        localStorage.setItem("checkoutComplete", "true");
         clearCart();
         navigate("/checkout/success");
     }
