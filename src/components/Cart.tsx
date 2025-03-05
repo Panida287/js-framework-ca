9ee27d1d1546dd6b30f1c9ffc5812ef2d23f7101
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-03-05 15:05:26 +0100

    feat: added cart total and checkout button
    - implemented cart total component
    - refactored CartItems component
    - added checkout button
    - add functionality so clear cart and checkout button only appear if there are items in cart

---
+++ src/components/Cart.tsx
@@ -0,0 +1,60 @@
+import {useCartStore} from "../store/cartStore";
+import {Link} from "react-router-dom";
+
+
+function CartTotal() {
+    const {cart} = useCartStore();
+
+    const totalPrice = cart.reduce((acc, product) =>
+        acc + (product.discountedPrice ? product.discountedPrice : product.price), 0);
+
+    return (
+        <div className="flex justify-between items-center p-4 border-t mt-4">
+            <h3 className="text-lg font-bold">Total:</h3>
+            <p className="text-lg font-semibold">${totalPrice.toFixed(2)}</p>
+        </div>
+    );
+}
+
+export function CartItems() {
+    const {cart, removeFromCart} = useCartStore();
+    return (
+        <div className="max-w-4xl mx-auto p-6">
+            {cart.length === 0 ?
+                (
+                    <p className="text-sm text-red-600 text-center">
+                        Your cart is empty.
+                    </p>
+                ) : (
+                    <>
+                        {cart.map((product) => (
+                            <div key={product.id} className="flex justify-between items-center p-4 border mb-2">
+                                <Link to={`/product/${product.id}`}>
+                                    <img
+                                        src={product.image.url}
+                                        alt={product.image?.alt || product.title}
+                                        className="object-cover h-20"
+                                    />
+                                </Link>
+                                <h3 className="text-lg font-bold">{product.title}</h3>
+                                {product.discountedPrice !== product.price ? (
+                                    <p>
+                                        Price: <del>${product.price}</del> ${product.discountedPrice}
+                                    </p>
+                                ) : (
+                                    <p>Price: ${product.price}</p>
+                                )}
+                                <button
+                                    onClick={() => removeFromCart(product.id)}
+                                    className="px-4 py-2 rounded bg-red-600 text-white"
+                                >
+                                    Remove from cart
+                                </button>
+                            </div>
+                        ))}
+                        <CartTotal/>
+                    </>
+                )}
+        </div>
+    );
+}
--- src/routes/CartPage/index.tsx
+++ src/routes/CartPage/index.tsx
@@ -1,57 +1,40 @@
-import { useCartStore } from "../../store/cartStore";
-import { Link } from "react-router-dom";
+import {CartItems} from "../../components/Cart";
+import {useCartStore} from "../../store/cartStore";
+import {Link} from "react-router-dom";
 
 export default function CartPage() {
-    const { cart, removeFromCart, clearCart } = useCartStore();
-
+    const {cart, clearCart} = useCartStore();
     return (
-        <div className="max-w-4xl mx-auto p-6">
-            <h2 className="flex justify-center items-center font-bold text-xl text-gray-800 leading-tight mb-4">
+        <>
+            <h1 className="flex justify-center items-center font-bold text-xl text-gray-800 leading-tight mb-4">
                 Cart
-            </h2>
-
-            {cart.length === 0 ? (
-                <p className="text-sm text-red-600 text-center">
-                    Your cart is empty.
-                </p>
-            ) : (
-                <>
-                    {cart.map((product) => (
-                        <div key={product.id} className="flex justify-between items-center p-4 border mb-2">
-                            <Link to={`/product/${product.id}`}>
-                                <img
-                                    src={product.image.url}
-                                    alt={product.image?.alt || product.title}
-                                    className="object-cover h-20"
-                                />
-                            </Link>
-                            <h3 className="text-lg font-bold">{product.title}</h3>
-                            {product.discountedPrice !== product.price ? (
-                                <p>
-                                    Price: <del>${product.price}</del> ${product.discountedPrice}
-                                </p>
-                            ) : (
-                                <p>Price: ${product.price}</p>
-                            )}
-                            <button
-                                onClick={() => removeFromCart(product.id)}
-                                className="px-4 py-2 rounded bg-red-600 text-white"
-                            >
-                                Remove from cart
-                            </button>
-                        </div>
-                    ))}
-
-                    <div className="flex justify-center mt-6">
+            </h1>
+            <CartItems/>
+            {cart.length > 0 ?
+                (
+                    <div className=" flex justify-between max-w-4xl mx-auto p-6">
                         <button
                             onClick={clearCart}
-                            className="px-6 py-3 bg-gray-800 text-white font-bold rounded hover:bg-gray-700 transition-all"
+                            className="px-6 py-3 bg-red-600 text-white font-bold rounded hover:bg-red-700 transition-all"
                         >
                             Clear Cart
                         </button>
+
+                        <Link to="/checkout"
+                              className="px-6 py-3 bg-gray-800 text-white font-bold rounded hover:bg-gray-700 transition-all">
+                            Check Out
+                        </Link>
                     </div>
-                </>
-            )}
-        </div>
+                ) : (
+                    <div className="flex justify-center p-6">
+                        <Link to="/"
+                              className="px-6 py-3 bg-gray-800 text-white font-bold rounded hover:bg-gray-700 transition-all"
+                        >
+                            Back to shopping
+                        </Link>
+                    </div>
+                )
+            }
+        </>
     );
 }
